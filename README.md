6c6c7bc19bf66367694dbe17cb01a431ed50499b
Jon Passki <jon@passki.us>
2015-12-04 13:39:25 -0800

    java.lang.ClassLoader.getResource

---
+++ README.md
@@ -0,0 +1,9 @@
+# Purpose
+
+Test out JRE and third-party APIs for different dataflow-related
+issues.
+
+This is a multi-module Maven project. Each third party API (think 
+JAR-level) has its own module. This allows one to only update a module
+without having to download an Internet-worth of dependencies.
+
---
+++ jre-1-8/src/test/java/us/coastalhacking/tests/java/lang/ClassLoader.java
@@ -0,0 +1,33 @@
+package us.coastalhacking.tests.java.lang;
+
+import static org.junit.Assert.*;
+
+import java.net.URL;
+
+import org.junit.Test;
+
+public class ClassLoader {
+
+	@Test
+	public void test_getResource_pathTraversal() {
+	  final String resourceName = "resource.txt";
+	  final String assetDir = "asset";
+	  final String garbage = "aeuaoeuaou";
+
+    // TN control
+    URL resource = getClass().getClassLoader().getResource("./" + garbage + "/" +
+                                                           resourceName);
+    assertNull(resource);
+
+	  // TP control
+	  resource = getClass().getClassLoader().getResource("./" + assetDir + "/" +
+	                                                     resourceName);
+	  assertNotNull(resource);
+	  
+	  // Test upwards and then downwards traversal
+	  resource = getClass().getClassLoader().getResource("./" + assetDir + "/" +
+	                                                     "../" + assetDir + "/" +
+	                                                     resourceName);
+	  assertNotNull(resource);
+	}
+}
